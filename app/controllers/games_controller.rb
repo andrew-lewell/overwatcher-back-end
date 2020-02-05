9b98013c32390cdb1f6dc8f4de443a6a64be95c3
andrew-lewell <andrew.lewell@gmail.com>
2020-02-05 17:33:20 +0000

    fixed order of returned games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
     before_action :protected_action
 
     def index
-        games = Game.all
+        games = Game.order(created_at: :asc)
 
         render json: games
     end 
