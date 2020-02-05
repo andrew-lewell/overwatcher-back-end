79f8474fee10722dab0dfb399eb6eb1c6643ed77
andrew-lewell <andrew.lewell@gmail.com>
2020-02-05 09:46:03 +0000

    mvp done

--- app/serializers/map_serializer.rb
+++
@@ -1,4 +0,0 @@
-class MapSerializer < ActiveModel::Serializer
-  attributes :id, :map, :map_type
-  # has_many :games
-end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   post '/signin', to: "users#signin"
   post '/validate', to: "users#validate"
   get '/herostats', to: "heros#herostats"
+  get '/winpercbyrole', to: "heros#winpercbyrole"
+  get '/winpercbymap', to: "maps#winpercbymap"
+  get '/winpercbymaptype', to: "maps#winpercbymaptype"
 
   resources :seasons
   resources :games
